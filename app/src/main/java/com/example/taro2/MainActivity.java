a7f4abab3889cda95f3426fbd638bedcd694ede0
Viner <Taratheliberator@gmail.com>
2023-07-11 14:22:47 +0500

    first commit

---
+++ app/src/main/java/com/example/taro2/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.taro2;
+
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView cardImageView;
+    private Button openCardButton;
+
+    private int[] tarotCards = {
+            R.drawable.card1,
+            R.drawable.card2,
+            R.drawable.card3,
+            R.drawable.card4,
+            R.drawable.card5,
+            R.drawable.card6,
+            R.drawable.card7,
+            R.drawable.card8
+            // ... добавьте остальные карты Таро
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cardImageView = findViewById(R.id.cardImageView);
+        openCardButton = findViewById(R.id.openCardButton);
+
+        openCardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRandomCard();
+            }
+        });
+    }
+
+    private void openRandomCard() {
+        Random random = new Random();
+        int randomIndex = random.nextInt(tarotCards.length);
+        int randomCard = tarotCards[randomIndex];
+
+        cardImageView.setImageResource(randomCard);
+        cardImageView.setVisibility(View.VISIBLE);
+    }
+}
+
